bdcc58893e7ce705ca5f4839dcae92bda0219814
EhsanFox <ehsan8184@gmail.com>
2022-11-09 14:52:06 +0330

    fix(types): Added Request type

--- src/auth/account.decorator.ts
+++ src/auth/account.decorator.ts
@@ -1,8 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { Request } from 'express';
 import { AccountDocument } from './interfaces';
 
 export const AccountDecorator = createParamDecorator((data: unknown, ctx: ExecutionContext): AccountDocument =>
 {
-    const request = ctx.switchToHttp().getRequest();
+    const request = ctx.switchToHttp().getRequest<Request>();
     return request.account;
 });
