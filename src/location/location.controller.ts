fb07dde11d5ed664e4d478a12f8382cf7768fc60
EhsanFox <ehsan8184@gmail.com>
2022-11-21 08:39:55 +0330

    feat: search by regex/keyword

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -9,7 +9,10 @@ import {
     Body,
     UseInterceptors,
     UseGuards,
+    Query,
 } from "@nestjs/common";
+import { CityNotFoundException } from "src/excepetions/CityNotFound.excp";
+import { CountryNotFoundException } from "src/excepetions/CountryNotFound.excp";
 import { AuthGuard } from "src/shared/auth.guard";
 import { CityDto } from "./dto/city.dto";
 import { CityOutputDto } from "./dto/cityOutput.dto";
@@ -20,6 +23,36 @@ import { LocationService } from "./locations.service";
 @Controller("location")
 export class LocationController {
     constructor(private readonly locationService: LocationService) {}
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Get("search/city")
+    async searchCity(
+        @Query("keyword") keyword: string,
+        @Query("english") isEnglishName = true
+    ): Promise<CityOutputDto[]> {
+        const list = await this.locationService.getCityByRegex(
+            keyword,
+            !isEnglishName
+        );
+        if (!list || !list.length) throw new CityNotFoundException();
+
+        return list.map((x) => new CityOutputDto(x));
+    }
+
+    @UseInterceptors(ClassSerializerInterceptor)
+    @Get("search/country")
+    async searchCountry(
+        @Query("keyword") keyword: string,
+        @Query("english") isEnglishName = true
+    ): Promise<CountryOutputDto[]> {
+        const list = await this.locationService.getCountryByRegex(
+            keyword,
+            !isEnglishName
+        );
+        if (!list || !list.length) throw new CountryNotFoundException();
+
+        return list.map((x) => new CountryOutputDto(x));
+    }
 
     @UseInterceptors(ClassSerializerInterceptor)
     @Get("city")
@@ -37,7 +70,7 @@ export class LocationController {
 
     @UseGuards(AuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    @Post("city/create")
+    @Post("create/city")
     async createCity(@Body() cityDto: CityDto): Promise<CityOutputDto> {
         return new CityOutputDto(
             await this.locationService.registerCity(cityDto)
@@ -46,7 +79,7 @@ export class LocationController {
 
     @UseGuards(AuthGuard)
     @UseInterceptors(ClassSerializerInterceptor)
-    @Post("country/create")
+    @Post("create/country")
     async createCountry(
         @Body() countryDto: CountryDto
     ): Promise<CountryOutputDto> {
